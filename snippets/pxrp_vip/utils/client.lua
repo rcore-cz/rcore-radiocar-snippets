0f82c33fd99305319166695d907a87194067124c
MrXogos <49158267+Xogy@users.noreply.github.com>
2021-06-14 10:04:04 +0200

    formated pxrp_vip

--- snippets/pxrp_vip/utils/client.lua
+++ snippets/pxrp_vip/utils/client.lua
@@ -2,31 +2,31 @@ ESX = nil
 local isVip = false
 
 CreateThread(function()
-	local breakMe = 0
+    local breakMe = 0
     while ESX == nil do
         Wait(100)
-		breakMe = breakMe + 1
+        breakMe = breakMe + 1
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		if breakMe > 10 then
-			break
-		end
+        if breakMe > 10 then
+            break
+        end
     end
 end)
 
 CreateThread(function()
-	ESX.TriggerServerCallback('pxrp_vip:getVIPStatus', function(vip)
-		isVip = vip
-	end, GetPlayerServerId(PlayerId()), '1')
+    ESX.TriggerServerCallback('pxrp_vip:getVIPStatus', function(vip)
+        isVip = vip
+    end, GetPlayerServerId(PlayerId()), '1')
 end)
 
 -- this will send information to server.
 function CheckPlayerCar(vehicle)
-	if ESX then
-		local veh = ESX.Game.GetVehicleProperties(vehicle)
-		TriggerServerEvent("rcore_radiocar:openUI", veh.plate)
-	else
-		TriggerServerEvent("rcore_radiocar:openUI", GetVehicleNumberPlateText(vehicle))
-	end
+    if ESX then
+        local veh = ESX.Game.GetVehicleProperties(vehicle)
+        TriggerServerEvent("rcore_radiocar:openUI", veh.plate)
+    else
+        TriggerServerEvent("rcore_radiocar:openUI", GetVehicleNumberPlateText(vehicle))
+    end
 end
 
 -- if you want this script for... lets say like only vip, edit this function.
